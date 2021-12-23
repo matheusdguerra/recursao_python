d1a2ebb72851676df8737a5bdd9d8ef456dddf56
matheus.guerra <matheusguerra@outlook.com>
2021-12-23 18:10:39 -0300

    Execução de exercícios 23/12/2021

--- Exercicios_Recursao.py
+++ Exercicios_Recursao.py
@@ -1,1 +1,9 @@
 # Testes com Recursão
+'''
+https://www.youtube.com/watch?v=fbj5Y19x0fs
+https://www.youtube.com/watch?v=0PwFwqiNfAI
+
+https://panda.ime.usp.br/pensepy/static/pensepy/12-Recursao/recursionsimple-ptbr.html
+https://academiahopper.com.br/recursao-python/
+http://devfuria.com.br/logica-de-programacao/recursividade-e-algoritmos-recursivos/
+'''
